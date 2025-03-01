0bc6a7b75827a43e58496c99427d4ada6d6a4a41
Gustavo Miranda <gustmrg@gmail.com>
2025-03-01 14:14:24 -0300

    feat: add home translation

--- src/pages/home/components/cta-button.tsx
+++ src/pages/home/components/cta-button.tsx
@@ -1,11 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { NavLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 export function CTAButton() {
+  const { t } = useTranslation();
+
   return (
     <NavLink to="/auth/sign-up">
       <Button className="flex flex-row space-x-2 gap-2 font-semibold text-white bg-blue-600 hover:bg-blue-500 shadow-sm">
-        Get started
+        {t("home.cta")}
       </Button>
     </NavLink>
   );
