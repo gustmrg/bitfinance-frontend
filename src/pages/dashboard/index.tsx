2dfd92782aba2d93f105941ddf5f06a512ac4a72
Gustavo Miranda <gustmrg@gmail.com>
2025-03-02 13:34:59 -0300

    fix: stop infinite dashboard re-rendering

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -66,7 +66,7 @@ export function Dashboard() {
 
     fetchUpcomingBills();
     fetchRecentExpenses();
-  }, [selectedOrganization, bills, expenses]);
+  }, [selectedOrganization]);
 
   return (
     <div className="flex-1 space-y-4 p-8 pt-6">
