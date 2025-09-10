a84f97368ff935945ca23b0d10609b4667de28dc
Gustavo Miranda <gmiranda90@outlook.com>
2025-09-09 23:32:55 -0300

    fix: add base path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,10 @@ import "./index.css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
-const router = createRouter({ routeTree });
+const router = createRouter({ 
+  routeTree,
+  basepath: "/bitfinance"
+});
 
 declare module "@tanstack/react-router" {
   interface Register {
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Home } from '@/pages/home'
+import { Home } from "@/pages/home";
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: Home,
-})
+});
